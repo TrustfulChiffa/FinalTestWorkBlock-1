fcebfb1264e65ecc17a5c118dad7dc246dd844bd
Chiffa <babiy99021422@gmail.com>
2023-02-19 00:26:33 +0300

    Создал войд для перезапии нужных элементов первого массива во второй массив

--- task/Program.cs
+++ task/Program.cs
@@ -1,3 +1,15 @@
 ﻿string[] arrayAnySize = new string[6] {"q", "qw", "qwe", "qwerty", "geek", "xyz"};
 string[] arraySizeOf3 = new string[arrayAnySize.Length];
 
+void SelectLinesLess4(string[] arrayAnySize, string[] arraySizeOf3)
+{
+    int count = 0;
+    for (int i = 0; i < arrayAnySize.Length; i++)
+    {
+    if(arrayAnySize[i].Length <= 3)
+        {
+        arraySizeOf3[count] = arrayAnySize[i];
+        count++;
+        }
+    }
+}
