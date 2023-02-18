9bef0caa792bfc55ea6337b45cabb83f0f1e58cc
Chiffa <babiy99021422@gmail.com>
2023-02-19 00:28:43 +0300

    Проверил, что всё работает и форматировал код для красоты

--- task/Program.cs
+++ task/Program.cs
@@ -1,4 +1,4 @@
-﻿string[] arrayAnySize = new string[6] {"q", "qw", "qwe", "qwerty", "geek", "xyz"};
+﻿string[] arrayAnySize = new string[6] { "q", "qw", "qwe", "qwerty", "geek", "xyz" };
 string[] arraySizeOf3 = new string[arrayAnySize.Length];
 
 void SelectLinesLess4(string[] arrayAnySize, string[] arraySizeOf3)
@@ -6,10 +6,10 @@ void SelectLinesLess4(string[] arrayAnySize, string[] arraySizeOf3)
     int count = 0;
     for (int i = 0; i < arrayAnySize.Length; i++)
     {
-    if(arrayAnySize[i].Length <= 3)
+        if (arrayAnySize[i].Length <= 3)
         {
-        arraySizeOf3[count] = arrayAnySize[i];
-        count++;
+            arraySizeOf3[count] = arrayAnySize[i];
+            count++;
         }
     }
 }
